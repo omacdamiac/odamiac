552382578bae8ba43571434bdf53f632ac1eb7e2
Omac damiac <omac.damiac@gmail.com>
2018-08-23 02:46:56 -0500

    error al compilar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { LeftComponent } from './left/left.component';
+import { CollageComponent } from './collage/collage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    LeftComponent,
+    CollageComponent
   ],
   imports: [
     BrowserModule
